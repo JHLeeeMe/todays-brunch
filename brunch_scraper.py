5be8d5762b345e45c351f5bc6af2ac4d28c0d5c2
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-08 02:53:46 +0900

    [Add] 스크립트 실행파일
    	새 파일:       brunch_scraper.py
    	새 파일:       main.py

---
+++ brunch_scraper.py
@@ -0,0 +1,69 @@
+import bs4
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from pytz import timezone
+from datetime import datetime
+from typing import List
+
+
+Tag = bs4.element.Tag
+
+def get_title(category: str) -> str:
+    issue_title = "[" + category + "] Today's brunch - " + \
+            datetime.now(tz=timezone('Asia/Seoul')).strftime('%Y.%m.%d')
+    return issue_title
+
+def get_body(category: str) -> str:
+    issue_body = ''
+    for e in get_tags(category):
+        publish_time = e.find('span', {'class': 'publish_time'}).text
+
+        if not is_today(publish_time):
+            break
+
+        href = 'https://brunch.co.kr' + e['href']
+        author = 'by ' + e.find_all('span', {'class': 'name_txt'})[-1].text
+        title = e.find('strong').text
+        content = e.find('span', {'class': 'article_content'}).text + ' ...'
+
+        issue_body += '<h1>' + title + \
+                '</h1><a href="' + href + '">' + href + '</a><br><br>' + \
+                content + \
+                '<h3><p align="right">' + author + '</h3></p><br><br><br>'
+
+    return issue_body
+
+def get_tags(category: str) -> List[Tag]:
+    # headless option
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+
+    driver = webdriver.Chrome('./web-drivers/chromedriver', options=options)
+
+    url = 'https://brunch.co.kr/keyword/' + category + '?q=g'
+    driver.get(url)
+    elem = driver.find_element_by_tag_name('body')
+    for i in range(5):
+        elem.send_keys(Keys.END)
+        time.sleep(1)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    a_tags = soup.body.find_all('a', {'class': 'link_post'})
+
+    driver.quit()
+
+    return a_tags
+
+def is_today(publish_time: str) -> bool:
+    # '*시간전' or '*분전'
+    if len(publish_time) < 6:
+        return True
+
+    today = datetime.now(tz=timezone('Asia/Seoul')).strftime('%d')
+    pub = datetime.strptime(publish_time, '%b %d. %Y').strftime('%d')
+
+    return today == pub
---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from github import Github
+from brunch_scraper import get_title, get_body
+
+if __name__ == '__main__':
+    try:
+        GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+        REPO_NAME = 'todays-brunch'
+        CATEGORY = 'IT 트렌드'
+        issue_title = get_title(CATEGORY)
+        issue_body = get_body(CATEGORY)
+
+        if issue_body == '':
+            print('There is no updated brunch.')
+            sys.exit()
+
+        repo = Github(GITHUB_TOKEN).get_user().get_repo(REPO_NAME)
+        res = repo.create_issue(title=issue_title, body=issue_body)
+        print('Success!')
+        print(res)
+    except Exception as e:
+        print(e)
